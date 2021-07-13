c09ad3e3fcb77514288acafcd793259d08e8ae46
AlanRicardoS <alan.ricardo.silva1@gmail.com>
2021-07-13 18:19:42 -0300

    adicionando classe user

---
+++ microsservico/src/main/java/com/example/microsservico/model/User.java
@@ -0,0 +1,45 @@
+package com.example.microsservico.model;
+
+import javax.persistence.*;
+
+/**
+ * @author Alan Ricardo
+ */
+@Entity
+@Table(name = "user_tbl")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+}
